49be80c81390322065ecd6b64ea8bf584025973b
mauroslucios <mauroslucios@gmail.com>
2025-09-28 20:25:52 -0300

    melhorando logs

--- logs/etl_logger.py
+++ logs/etl_logger.py
@@ -8,41 +8,46 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-
 class ETLLogger:
     @staticmethod
-    def log_event(event, db=None, table=None, action=None, result=None, error=None):
+    def log_event(event, db=None, table=None, action=None, result=None, error=None, extra=None, level="INFO"):
         """
         Cria um log detalhado do evento do ETL.
         """
-        msg = f"[{event}] "
+        msg = f"[{event}]"
 
         if db:
-            msg += f"Database={db} "
+            msg += f" | Database: {db}"
         if table:
-            msg += f"Table={table} "
+            msg += f" | Table: {table}"
         if action:
-            msg += f"Action={action} "
+            msg += f" | Action: {action}"
         if result:
-            msg += f"Result={result} "
+            msg += f" | Result: {result}"
+        if extra:
+            msg += f" | Extra: {extra}"
         if error:
-            msg += f"Error={error} "
+            msg += f" | Error: {error}"
 
-        if error:
+        if level == "ERROR":
             logging.error(msg)
+        elif level == "WARNING":
+            logging.warning(msg)
         else:
             logging.info(msg)
 
     @staticmethod
-    def log_exception(event, db=None, table=None, action=None, exc=None):
+    def log_exception(event, db=None, table=None, action=None):
         """
-        Atalho para logar exceções com stacktrace.
+        Loga exceções automaticamente com stacktrace.
         """
+        error_msg = traceback.format_exc()
         ETLLogger.log_event(
             event=event,
             db=db,
             table=table,
             action=action,
             result="Erro",
-            error=traceback.format_exc() if exc else None,
+            error=error_msg,
+            level="ERROR"
         )
