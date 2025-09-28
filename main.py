faf3ef8f940aabd48ae4d9f58388e1db56605248
mauroslucios <mauroslucios@gmail.com>
2025-09-28 20:11:15 -0300

    aqruivo principal da aplicação

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pyspark.sql import SparkSession
 import traceback
 
-from etl.logger import ETLLogger
-from etl.extractor import Extractor
-from etl.transformer import Transformer
-from etl.loader import Loader
-from etl.config import S3_BUCKET, S3_PREFIX, MAX_FILE_SIZE_BYTES
+from logs.etl_logger import ETLLogger
+from util.extractor import Extractor
+from util.transformer import Transformer
+from util.loader import Loader
+from util.config import S3_BUCKET, S3_PREFIX, MAX_FILE_SIZE_BYTES
 
 def main():
     ETLLogger.log_event("ETL iniciado")
@@ -27,14 +27,14 @@ def main():
         df_prop = extractor.extract_table("propriedades")
         df_neg = extractor.extract_table("negativacao")
 
-        df_decisao = transformer.transform_business_rule(df_cliente, df_endereco, df_prop, df_neg)
+        df_decisao = transformer.transform_business_rule(df_cliente, df_endereco, df_prop, df_neg) # noqa
         df_decisao.show(5, truncate=False)
 
-        loader.load_as_chunked_json_to_s3(df_decisao, S3_BUCKET, S3_PREFIX, MAX_FILE_SIZE_BYTES)
+        loader.load_as_chunked_json_to_s3(df_decisao, S3_BUCKET, S3_PREFIX, MAX_FILE_SIZE_BYTES) # noqa
 
         ETLLogger.log_event("ETL finalizado", result="Sucesso")
     except Exception:
-        ETLLogger.log_event("ETL finalizado", result="Erro", error=traceback.format_exc())
+        ETLLogger.log_event("ETL finalizado", result="Erro", error=traceback.format_exc()) # noqa
         raise
     finally:
         spark.stop()
